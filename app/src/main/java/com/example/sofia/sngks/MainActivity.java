9aefe3c73a758c3afda314987898d3e1cd3800a7
Regynald Ventura <regynaldventura@gmail.com>
2016-06-10 17:34:06 +0800

    Initial Android Commit

---
+++ app/src/main/java/com/example/sofia/sngks/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sofia.sngks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
